b6296ce822a04593adf80ea1bfa01c67b18c76c1
Fabian Herrera <contactofabianmartinez@gmail.com>
2025-01-07 00:08:42 -0600

    refactor: Update FAQ component styles and improve accessibility

--- src/modules/sponsor/FAQ.tsx
+++ src/modules/sponsor/FAQ.tsx
@@ -1,4 +1,5 @@
-import { Accordion, AccordionItem } from '@nextui-org/react'
+import React from 'react'
+import { Accordion, AccordionItem } from "@nextui-org/accordion"
 import { FAQS } from '@/constants/FAQ'
 
 export default function FAQ() {
@@ -12,22 +13,22 @@ export default function FAQ() {
 		>
 			<div className='mx-auto grid max-w-[40rem] grid-cols-1 gap-x-14 gap-y-16 lg:max-w-none lg:grid-cols-12'>
 				<div className='lg:col-span-4'>
-					<h2 className='text-lg leading-7 text-transparent bg-clip-text bg-gradient-to-b from-[#04B4E6] to-blue-600 font-GilroySemibold'>
+					<h2 className='text-lg leading-7 text-transparent bg-clip-text bg-gradient-to-r from-[#93603e] via-[#c18156] to-[#cea472] font-GilroySemibold'>
 						Frequently asked questions
 					</h2>
-					<p className='mt-4 text-4xl font-GilroyBold tracking-tight text-black dark:text-white xl:text-6xl xl:leading-[3.5rem]'>
+					<p className='mt-4 text-4xl font-GilroyBold tracking-tight text-black xl:text-6xl xl:leading-[3.5rem]'>
 						Everything you need to know about wind UI
-						<span className='text-transparent bg-clip-text bg-gradient-to-b from-[#04B4E6] to-blue-600'>
+						<span className='text-transparent bg-clip-text bg-gradient-to-r from-[#93603e] via-[#c18156] to-[#cea472] font-GilroySemibold'>
 							.
 						</span>
 					</p>
 				</div>
 				<div className='-mb-4 space-y-12 lg:col-span-8 xl:col-span-7 xl:col-start-6'>
 					<section>
-						<Accordion className='text-black dark:text-white'>
+						<Accordion className='text-black'>
 							{FAQS.map((faq) => (
 								<AccordionItem
-									className='text-black dark:text-white font-GilroyRegular'
+									className='text-black font-GilroyRegular'
 									classNames={{ title: 'text-danger, font-GilroySemibold' }}
 									key={faq.key}
 									aria-label={faq.label}
